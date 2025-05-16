94cba0edae2af02a8286f0422cd8c51a3209bdfb
David <davidbrundukov228@gmail.com>
2025-05-16 12:18:14 +0300

    not unpack item

--- collect_recommendations.py
+++ collect_recommendations.py
@@ -216,7 +216,7 @@ def collect_recommendations():
         with Pool(processes=2) as pool:
             result = []
             for item in to_pool:
-                result.append(pool.apply_async(inner_function, item))
+                result.append(pool.apply_async(inner_function, (item, )))
             for res in result:
                 try:
                     res.get(timeout=30)
